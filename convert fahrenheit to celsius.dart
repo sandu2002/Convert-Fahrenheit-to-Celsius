a22a1d9246e227098e1097942f27cd536247c7f4
Sandupama <94014107+sandu2002@users.noreply.github.com>
2024-07-01 11:49:03 +0530

    Create convert fahrenheit to celsius.dart
    Create convert fahrenheit to celsius.dart

---
+++ convert fahrenheit to celsius.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  //Convert Fahrenheit to Celsius
+  
+  double F = 0;
+  double C = 0;
+  
+  //equation to convert F in to c
+  
+  C = (F-32)*5/9;
+  
+  print("Celcius value of $F Farrenheit is $C celcius");
+ 
+}  
